fdbcca094ed2c30a6c06289e05fc0c680c34a1fc
jullev <setiyawanjullev@gmail.com>
2017-01-30 13:47:09 +0700

    Tambah item

--- app/src/main/java/com/cloud/demo/ajk_riset/demo_cloud/MainActivity.java
+++ app/src/main/java/com/cloud/demo/ajk_riset/demo_cloud/MainActivity.java
@@ -2,12 +2,27 @@ package com.cloud.demo.ajk_riset.demo_cloud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button tombol1,tombol2;
+    EditText Isian1,Isian2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tombol1 = (Button) findViewById(R.id.button);
+        tombol2 = (Button) findViewById(R.id.button2);
+        Isian1 = (EditText) findViewById(R.id.editText);
+        Isian2 = (EditText) findViewById(R.id.editText2);
+        tombol1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
